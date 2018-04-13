53ac27956264836ad398644acc173429e7781dfd
jseidman95 <seidmanjesse@gmail.com>
2018-04-13 13:27:18 -0400

    Initial Commit

---
+++ JSTextTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSTextTableView
+//
+//  Created by Jesse Seidman on 4/13/18.
+//  Copyright © 2018 Jesse Seidman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
