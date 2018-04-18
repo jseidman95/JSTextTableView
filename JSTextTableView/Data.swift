2fd50a416dc103dd72e1932e547634a192d51cf1
jseidman95 <seidmanjesse@gmail.com>
2018-04-18 15:19:13 -0400

    added data file

---
+++ JSTextTableView/Data.swift
@@ -0,0 +1,16 @@
+//
+//  Data.swift
+//  JSTextTableView
+//
+//  Created by Jesse Seidman on 4/13/18.
+//  Copyright © 2018 Jesse Seidman. All rights reserved.
+//
+
+import UIKit
+
+public struct CellData
+{
+  public var text:NSAttributedString
+  public var isExpandable:Bool
+  public var isExpanded:Bool
+}
